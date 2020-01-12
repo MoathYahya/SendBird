e406270985af27a1ee61d12b3adeb8fe567252a5
moathyahya <mo3ad_y@hotmail.com>
2020-01-12 11:57:53 -0800

    Initial Commit

---
+++ SendBirdTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SendBirdTask
+//
+//  Created by Moath Yahia on 1/12/20.
+//  Copyright © 2020 Moath Yahia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
